5bc81ca6cda52d5a3130e4d691c28e8bacab7a79
vaibhav315 <137273403+vaibhav315@users.noreply.github.com>
2025-06-01 15:05:43 +0530

    Add files via upload

---
+++ MyFirst.java
@@ -0,0 +1,59 @@
+import java.lang.*;
+import java.util.*;
+class MyFirst 
+{
+      public static void main (String args[])
+      {
+            // System.out.println("Int Min : " + Integer.MIN_VALUE);
+            // System.out.println("Int Max : " + Integer.MAX_VALUE);
+            // System.out.println("Int Bytes : " + Integer.BYTES);
+            
+
+            //    String name;
+            // Scanner sc=new Scanner(System.in);
+            // System.out.println("May I know your name ?");
+            // name=sc.nextLine();
+
+            // System.out.println("Welcome "+name);
+            // byte b = 5;
+            // System.out.println(b);
+            // b = 127;
+            // System.out.println(b);
+            // short s = 300;
+            // int i = 100;
+            // float f = 25.3F;
+            // char c = 'V';/
+            // byte b1 = 10;
+            // byte b2 = 0b1010;
+            // byte b3 = 012;
+            // byte b4 = 0XA;
+            // long l = 345345_345345L;
+            // int x=-5;
+            // System.out.println(x);
+            // System.out.println(l);
+            // System.out.println(b1);
+            // System.out.println(b2);
+            // System.out.println(b3);
+            // System.out.println(b4);
+
+            // System.out.println(s);
+            // System.out.println(i);
+            // System.out.println(f);
+            // System.out.println(c);
+
+            // int x=5,y;
+            // y=++x;
+            // System.out.println(x+" "+y);
+
+
+            int x=10, y=15;
+            x=x^y;
+            y=x^y;
+            x=x^y;
+            System.out.println("The values after swapping are x = " + x + " and y = " + y);
+
+            
+      
+
+      }
+}
